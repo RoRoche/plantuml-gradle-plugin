63ca4a70838a33c4ec5236821c8578b6c2d8bcc6
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-06 21:56:15 +0200

    add javadoc

--- src/main/kotlin/com/github/roroche/plantuml/classes/exceptions/InvalidPackageException.kt
+++ src/main/kotlin/com/github/roroche/plantuml/classes/exceptions/InvalidPackageException.kt
@@ -1,5 +1,10 @@
 package com.github.roroche.plantuml.classes.exceptions
 
+/**
+ * Exception to be thrown when a pckage is invalid.
+ *
+ * @param packageName The name of the package.
+ */
 class InvalidPackageException(
     packageName: String
 ) : RuntimeException("Invalid package '$packageName', maybe missing or empty")

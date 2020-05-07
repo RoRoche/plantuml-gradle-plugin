01c6903892745bc4c4f62035717644c51e2e23f5
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-07 10:47:00 +0200

    introduce ReflectionsHelper

--- src/main/groovy/com/github/roroche/plantuml/tasks/BuildClassDiagramTask.groovy
+++ src/main/groovy/com/github/roroche/plantuml/tasks/BuildClassDiagramTask.groovy
@@ -1,5 +1,6 @@
 package com.github.roroche.plantuml.tasks
 
+import com.github.roroche.plantuml.ReflectionsHelper
 import com.github.roroche.plantuml.classes.*
 import com.github.roroche.plantuml.diagrams.ClassDiagram
 import com.github.roroche.plantuml.diagrams.Diagram
@@ -42,6 +43,7 @@ class BuildClassDiagramTask extends DefaultTask implements CustomTask {
                 "Classes to ignore: {}",
                 extension.ignoredClasses
         )
+        ReflectionsHelper.registerUrlTypes()
         final ClassLoader classLoader = getClassLoader()
         final Classes classes = new ClsWithLog(
                 new ClsFiltered(

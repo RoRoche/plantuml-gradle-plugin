d87c12bd605e66d3e065148c02c206dbdb65ee71
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-07 13:43:07 +0200

    trying to configure urls

--- src/main/groovy/com/github/roroche/plantuml/tasks/BuildClassDiagramTask.groovy
+++ src/main/groovy/com/github/roroche/plantuml/tasks/BuildClassDiagramTask.groovy
@@ -5,7 +5,12 @@ import com.github.roroche.plantuml.diagrams.ClassDiagram
 import com.github.roroche.plantuml.diagrams.Diagram
 import com.github.roroche.plantuml.diagrams.DiagramWithLog
 import org.gradle.api.DefaultTask
+import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.TaskAction
 
 import javax.inject.Inject
@@ -42,6 +47,10 @@ class BuildClassDiagramTask extends DefaultTask implements CustomTask {
                 "Classes to ignore: {}",
                 extension.ignoredClasses
         )
+        def objectProvider = project.getConfigurations().named(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME)
+        getLogger().lifecycle(
+                "object provider: " + objectProvider
+        )
         final ClassLoader classLoader = getClassLoader()
         final Classes classes = new ClsWithLog(
                 new ClsFiltered(
@@ -65,14 +74,21 @@ class BuildClassDiagramTask extends DefaultTask implements CustomTask {
 
     @Internal
     protected ClassLoader getClassLoader() {
-        final URL[] urls = project.sourceSets.main.output.classesDirs.files.collect {
+        final List<URL> mutableUrls = new ArrayList()
+        final URL[] sourceSetsClassDirs = project.sourceSets.main.output.classesDirs.files.collect {
             if (it != null) {
                 it.toURI().toURL()
             }
         }.findAll {
             it != null
         } as URL[]
-        getLogger().debug(
+        getLogger().lifecycle(
+                "sourceSetsClassDirs URLs to scan: " + sourceSetsClassDirs
+        )
+        mutableUrls.addAll(sourceSetsClassDirs)
+
+        final URL[] urls = mutableUrls as URL[]
+        getLogger().lifecycle(
                 "URLs to scan: " + urls
         )
         return new URLClassLoader(urls)

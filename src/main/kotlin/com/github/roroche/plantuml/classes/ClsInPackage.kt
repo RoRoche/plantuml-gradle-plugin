b68cf95893e4c57c5dccffaf652387ad2920e9e7
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-07 04:22:26 +0200

    restore reflections

--- src/main/kotlin/com/github/roroche/plantuml/classes/ClsInPackage.kt
+++ src/main/kotlin/com/github/roroche/plantuml/classes/ClsInPackage.kt
@@ -1,9 +1,12 @@
-@file:Suppress("UnstableApiUsage")
-
 package com.github.roroche.plantuml.classes
 
 import com.github.roroche.plantuml.classes.exceptions.InvalidPackageException
-import com.google.common.reflect.ClassPath
+import org.reflections.Configuration
+import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
+import org.reflections.scanners.TypeAnnotationsScanner
+import org.reflections.util.ClasspathHelper
+import org.reflections.util.ConfigurationBuilder
 import java.net.URL
 import java.net.URLClassLoader
 
@@ -11,12 +14,26 @@ import java.net.URLClassLoader
  * Utility class to find [Classes] in a given package.
  *
  * @property packageName The name of the package to scan.
- * @property classPath Utility to find classes in package.
+ * @property reflections Utility to find classes in package.
  */
 class ClsInPackage(
     private val packageName: String,
-    private val classPath: ClassPath
+    private val reflections: Reflections
 ) : Classes {
+
+    /**
+     * Secondary constructor with reflections configuration.
+     *
+     * @param packageName The name of the package to scan.
+     * @param configuration The [Configuration] to use.
+     */
+    constructor(
+        packageName: String,
+        configuration: Configuration
+    ) : this(
+        packageName = packageName,
+        reflections = Reflections(configuration)
+    )
 
     /**
      * Secondary constructor.
@@ -29,7 +46,15 @@ class ClsInPackage(
         classLoader: ClassLoader
     ) : this(
         packageName = packageName,
-        classPath = ClassPath.from(classLoader)
+        configuration = ConfigurationBuilder(
+        ).setUrls(
+            ClasspathHelper.forClassLoader(classLoader)
+        ).setScanners(
+            SubTypesScanner(false),
+            TypeAnnotationsScanner()
+        ).addClassLoader(
+            classLoader
+        )
     )
 
     /**
@@ -55,19 +80,27 @@ class ClsInPackage(
         packageName: String
     ) : this(
         packageName = packageName,
-        classLoader = Thread.currentThread().contextClassLoader
+        reflections = Reflections(
+            ConfigurationBuilder()
+                .setUrls(
+                    ClasspathHelper.forPackage(packageName)
+                ).setScanners(
+                    SubTypesScanner(false),
+                    TypeAnnotationsScanner()
+                )
+        )
     )
 
     /**
      * @return Classes to be used for diagram generation.
      */
     override fun list(): List<Class<out Any>> {
-        return classPath.getTopLevelClasses(
-            packageName
-        ).map { classInfo ->
-            classInfo.load()
-        }.ifEmpty {
+        val list = reflections.getSubTypesOf(
+            Any::class.java
+        ).asIterable().toList()
+        if (list.isNullOrEmpty()) {
             throw InvalidPackageException(packageName)
         }
+        return list
     }
 }
